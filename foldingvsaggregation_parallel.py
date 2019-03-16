72b4c91c24cae2808c6b75dc42067ec01662cce1
Jason Bertram <jxb@h2.carbonate.uits.iu.edu>
2019-03-15 21:16:22 -0400

    parallelize

--- foldingvsaggregation_parallel.py
+++ foldingvsaggregation_parallel.py
@@ -1,24 +1,32 @@
+from scipy.sparse import csgraph
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import pathos.multiprocessing as mp
 import time
 from function_definitions import *
 
-L=48
+L=48-1
 H=0.5
 sample_size=100
 
 sequence=generate_initial_sequence(L,H)
 
+#for parallelization
+def Fpar(x):
+	return F(x,sample_size)
+
+pool=mp.Pool(12)
 start=time.time()
 
 sequence_history=[]
 number_beneficial_history=[]
 mutation_history=[]
-for i in range(100):
+for i in range(1000):
         print "Mutation number: ", i
         sequence_history.append(sequence)
-    	Finitial=F(sequence,sample_size)
-        mutation_effects=np.array(map(lambda x: (F(mutate(sequence,x),sample_size)-Finitial)/Finitial, range(L)))
+        Fvalues=np.array(pool.map(Fpar, [mutate(sequence,_) for _ in range(L)]+[sequence]))
+	mutation_effects=(Fvalues[:-1]-Fvalues[-1])/Fvalues[-1]
         beneficial_mutations=[position for position,_ in enumerate(mutation_effects) if _[0]>=0 and _[1]<=0]# and _[1]<=0]
         if len(beneficial_mutations)>0:
             #pick random beneficial mutation and mutate
@@ -29,20 +37,20 @@ for i in range(100):
             mutation_history.append(mutation_effects[beneficial_mutations[mutation_position]])
             print 'Mutation effect: ', mutation_history[-1]
             sequence=mutate(sequence,beneficial_mutations[mutation_position])
-        else:
+        #else:
             #weakly_deleterious_mutations=[position for position,_ in enumerate(mutation_effects) if _[0]>=-0.01 and _[1]<=0]
             #if len(weakly_deleterious_mutations)>0:
             #    mutation_position=np.random.randint(len(weakly_deleterious_mutations))
             #    print len(weakly_deleterious_mutations),"deleterious"
             #    sequence=mutate(sequence,weakly_deleterious_mutations[mutation_position])
             #else:
-            break
+            #break
 
 print (time.time()-start)/60.
-"""
+
 import pickle
-with open("/N/dc2/scratch/jxb/HPzipper",'w') as fout:
+with open("/N/dc2/scratch/jxb/HPzipper/output",'w') as fout:
     pickle.dump(sequence_history,fout)
     pickle.dump(number_beneficial_history,fout)
     pickle.dump(mutation_history,fout)
-"""
+
