5883c4c1cd8c42c5613381c186ef08fa1e3dd1e8
Jason Bertram <jxb@iu.edu>
2019-07-15 10:07:39 -0400

    latest

--- figures.py
+++ figures.py
@@ -22,7 +22,10 @@ def mutation_percentile(index):
 def path_length(structure_history):
     F_history=np.sum(np.array(structure_history)[:,:2],1)
     #return F_history[1:]-F_history[:-1]
-    return [pos for pos,_ in enumerate(F_history[1:]-F_history[:-1]) if _>0][-1]+1
+    if len(structure_history)>1:
+        return [pos for pos,_ in enumerate(F_history[1:]-F_history[:-1]) if _>0][-1]+1
+    else:
+        return 0
     
 def scale_points(x,y):
     tuples=zip(x,y)
@@ -108,7 +111,7 @@ ax1.scatter(A_pl[:,0]/L,A_pl[:,1]/L,s=2*A_pl[:,2],zorder=1)
 ax1.set_xlim([-2,0.])
 ax1.set_ylim([-1,0.6])
 ax1.set_xlabel(r'Initial $\overline{F}$ / $L$',fontsize=12)
-ax1.set_ylabel(r'Final $\overline{F}$ / $L$',fontsize=12)
+ax1.set_ylabel(r'Final Fitness / $L$',fontsize=12)
 ax1.yaxis.set_label_coords(-0.22,0.5)
 ax1.annotate('a',[0.05,0.9],xycoords='axes fraction',fontsize=12)
 
@@ -116,20 +119,20 @@ ax1.annotate('a',[0.05,0.9],xycoords='axes fraction',fontsize=12)
 #Delta F vs path length
 
 delta=np.sum(final_structures[incomplete_pos,:2],1)-np.sum(initial_structures[incomplete_pos,:2],1)
-A_all=scale_points(np.array(map(len,sequence_all[incomplete_pos]))/L,delta/L)
+A_all=scale_points(np.array(map(path_length,structure_all[incomplete_pos]))/L,delta/L)
 ax2.scatter(A_all[:,0],A_all[:,1],s=2*A_all[:,2],zorder=0)
 
 delta=np.sum(final_structures[not_incomplete_pos,:2],1)-np.sum(initial_structures[not_incomplete_pos,:2],1)
-A_all=scale_points(np.array(map(len,sequence_all[not_incomplete_pos]))/L,delta/L)
+A_all=scale_points(np.array(map(path_length,structure_all[not_incomplete_pos]))/L,delta/L)
 ax2.scatter(A_all[:,0],A_all[:,1],s=2*A_all[:,2],zorder=0)
 
 delta=np.sum(final_structures[complete_pos,:2],1)-np.sum(initial_structures[complete_pos,:2],1)
-A_all=scale_points(np.array(map(len,sequence_all[complete_pos]))/L,delta/L)
+A_all=scale_points(np.array(map(path_length,structure_all[complete_pos]))/L,delta/L)
 ax2.scatter(A_all[:,0],A_all[:,1],s=2*A_all[:,2],zorder=0,c='C3')
 
 ax2.set_xlim([0,1])
 ax2.set_xlabel(r'Path length / $L$',fontsize=12)
-ax2.set_ylabel(r'$\Delta \overline{F}$ / $L$',fontsize=12)
+ax2.set_ylabel(r'$\Delta$ Fitness / $L$',fontsize=12)
 ax2.yaxis.set_label_coords(-0.22,0.5)
 ax2.annotate('b',[0.05,0.9],xycoords='axes fraction',fontsize=12)
 
@@ -150,7 +153,7 @@ ax3.scatter(hydro_pl[:,0],hydro_pl[:,1]/L,s=2*hydro_pl[:,2],zorder=1)
 #ax3.set_xlim([0.4,1.8])
 ax3.set_ylim([-1,0.6])
 ax3.set_xlabel(r'Initial Hydrophobicity',fontsize=12)
-ax3.set_ylabel(r'Final $\overline{F}$ / $L$',fontsize=12)
+ax3.set_ylabel(r'Final Fitness / $L$',fontsize=12)
 ax3.yaxis.set_label_coords(-0.22,0.5)
 ax3.annotate('c',[0.05,0.9],xycoords='axes fraction',fontsize=12)
 
@@ -170,7 +173,7 @@ ax4.scatter(hydro_pl[:,0],hydro_pl[:,1]/L,s=2*hydro_pl[:,2],zorder=1)
 
 ax4.set_ylim([-1,0.6])
 ax4.set_xlabel(r'Final Hydrophobicity',fontsize=12)
-ax4.set_ylabel(r'Final $\overline{F}$ / $L$',fontsize=12)
+ax4.set_ylabel(r'Final Fitness / $L$',fontsize=12)
 ax4.yaxis.set_label_coords(-0.22,0.5)
 ax4.annotate('d',[0.05,0.9],xycoords='axes fraction',fontsize=12)
 
@@ -215,7 +218,6 @@ plt.tight_layout(w_pad=-.5)
 #Mazes
 #===========================================================
 
-
 with open ('fold_degeneracy_properties_fixed','r') as fin:
     sequence_all=cPickle.load(fin)
     structure_all=cPickle.load(fin)
@@ -241,75 +243,107 @@ complete_pos=[pos for pos,_ in enumerate(chance_complete_final) if _==1.]
 
 L=float(len(sequence_all[0][0]))
 
-fig2, (ax1,ax2) = plt.subplots(nrows=1,ncols=2,figsize=[7.5,4.0])
+fig2, (ax1,ax2) = plt.subplots(nrows=2,ncols=1,figsize=[3.5,6.])
 #===========================================================
 #Hamming plot
 
-ax1.plot([0.25,32/L],[0.25,32/L],'k',zorder=-1)
+delta=np.sum(final_structures[incomplete_pos,:2],1)-np.sum(initial_structures[incomplete_pos,:2],1)
+#A_all=scale_points(np.array(map(path_length,structure_all[incomplete_pos]))/L,delta/L)
+A_all=scale_points(np.sum((initial_sequences - final_sequences)**2,1)[incomplete_pos]/L,delta/L)
+ax1.scatter(A_all[:,0],A_all[:,1],s=2*A_all[:,2],zorder=0)
 
-#hamming_all=scale_points(map(lambda x: len(x)-1,structure_all),np.sum((initial_sequences - final_sequences)**2,1))
-#ax1.scatter(hamming_all[:,0]/L,hamming_all[:,1]/L,s=1*hamming_all[:,2],zorder=1)
+delta=np.sum(final_structures[not_incomplete_pos,:2],1)-np.sum(initial_structures[not_incomplete_pos,:2],1)
+#A_all=scale_points(np.array(map(path_length,structure_all[not_incomplete_pos]))/L,delta/L)
+A_all=scale_points(np.sum((initial_sequences - final_sequences)**2,1)[not_incomplete_pos]/L,delta/L)
+ax1.scatter(A_all[:,0],A_all[:,1],s=2*A_all[:,2],zorder=0)
+
+delta=np.sum(final_structures[complete_pos,:2],1)-np.sum(initial_structures[complete_pos,:2],1)
+#A_all=scale_points(np.array(map(path_length,structure_all[complete_pos]))/L,delta/L)
+A_all=scale_points(np.sum((initial_sequences - final_sequences)**2,1)[complete_pos]/L,delta/L)
+ax1.scatter(A_all[:,0],A_all[:,1],s=2*A_all[:,2],zorder=0,c='C3')
+
+#ax2.set_xlim([0,1])
+#ax1.set_xlabel(r'Hamming distance / $L$',fontsize=12)
+ax1.set_ylabel(r'$\Delta$ Fitness / $L$',fontsize=12)
+ax1.yaxis.set_label_coords(-0.22,0.5)
+ax1.annotate('a',[0.05,0.9],xycoords='axes fraction',fontsize=12)
+
+#===========================================================
+#Hamming plot
+
+ax2.plot([0.25,32/L],[0.25,32/L],'k',zorder=-1)
 
 hamming_pl=scale_points(map(path_length,structure_all[complete_pos]),np.sum((initial_sequences - final_sequences)**2,1)[complete_pos])
-ax1.scatter(hamming_pl[:,0]/L,hamming_pl[:,1]/L,s=8*hamming_pl[:,2],c='r',zorder=1)
+ax2.scatter(hamming_pl[:,1]/L,hamming_pl[:,0]/L,s=8*hamming_pl[:,2],c='r',zorder=1)
 
-ax1.set_xlabel(r'Path Length / $L$',fontsize=12)
-ax1.set_ylabel(r'Hamming distance / $L$',fontsize=12)
+ax2.set_ylabel(r'Path Length / $L$',fontsize=12)
+ax2.set_xlabel(r'Hamming distance / $L$',fontsize=12)
+ax2.annotate('b',[0.05,0.9],xycoords='axes fraction',fontsize=12)
 
-
-#fig1, ax1 = plt.subplots(figsize=[3.5,3.5])
-#
-#maziness=np.array(map(lambda x: len(x)-1,structure_all[protein_like_pos]))/np.sum((initial_sequences - final_sequences)**2,1)[protein_like_pos].astype(float)
-##maziness_pl=scale_points(-initial_structures[protein_like_pos,1]/L,maziness)
-#maziness_pl=scale_points(map(hydrophobicity,initial_sequences[protein_like_pos]),maziness)
-#ax1.scatter(maziness_pl[:,0],maziness_pl[:,1],s=2*maziness_pl[:,2],zorder=0)
-#
-#ax1.set_xlabel(r'Initial Aggregation Potential / $L$',fontsize=12)
-#ax1.set_ylabel(r'Maziness',fontsize=12)
-
+fig2.tight_layout()
 
 #===========================================================
 #Backtracking
 #===========================================================
 
-long_path_pos=[_ for pos,_ in enumerate(protein_like_pos) if len(sequence_all[_])>50]
-#chance_complete_example=map(lambda x: chance_complete(x,1000),sequence_all[pos])
-
+long_path_pos=[_ for pos,_ in enumerate(complete_pos) if path_length(structure_all[_])>60]
 pos=long_path_pos[0]
+#chance_complete_example=map(lambda x: chance_complete(x,1000),sequence_all[pos][:path_length(structure_all[pos])+1])
 
 structure_data=np.array(structure_all[pos])[:,:-1]
 structure_data[:,1]=-structure_data[:,1]
 
-fig1, (ax1,ax2,ax3,ax4,ax5) = plt.subplots(nrows=5,ncols=1,figsize=[3.5,7.5])
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
-ax1.plot(structure_data)
-ax1.set_ylabel(r'$A$, $S$')
+fig3 = plt.figure(figsize=[6,4],constrained_layout=True)
+gs = gridspec.GridSpec(nrows=3, ncols=2, figure=fig3)
+
+ax1=fig3.add_subplot(gs[0,0])
+ax1.plot(structure_data/L)
+ax1.set_ylabel(r'$A/L$, $S/L$',fontsize=12)
 ax1.set_xticklabels([])
 
+ax2=fig3.add_subplot(gs[1,0])
 ax2.plot(chance_complete_example)
-ax2.set_ylabel('Chance complete')
+ax2.set_ylabel(r'$\%$ complete',fontsize=12)
 ax2.set_xticklabels([])
 
+ax3=fig3.add_subplot(gs[2,0])
 ax3.plot(map(hydrophobicity,sequence_all[pos]))
-ax3.set_ylabel('Hydrophobicity')
-ax3.set_xticklabels([])
+ax3.set_ylabel(r'Hydrophobicity',fontsize=12)
+ax3.set_ylim([0.5,1])
+ax3.set_yticklabels(['.5','','1'])
+ax3.set_xlabel(r'Substitution number',fontsize=12)
 
-ax4.plot(map(lambda x: dispersion_all_phases(x,3),sequence_all[pos]))
-ax4.set_ylabel('Clustering')
-ax4.set_xticklabels([])
+ax4=fig3.add_subplot((gs[:,1]))
+#ax4.plot(map(hydrophobicity,sequence_all[pos]))
+im=ax4.imshow(np.arctan(mutations[pos])/np.arctan(1e6),extent=[2,58,69,1])
+ax4.set_xlabel(r'Sequence position',fontsize=12)
+ax4.set_ylabel(r'Substitution number',fontsize=12)
+ax4.set_title(r'Mutation effects')
+ax4.xaxis.set_label_coords(0.5,-0.08)
+axins = inset_axes(ax4,
+                   width="100%",  # width = 5% of parent_bbox width
+                   height="5%",  # height : 50%
+                   loc='lower center',
+                   bbox_to_anchor=(0, -0.25, 1, 1),
+                   bbox_transform=ax4.transAxes,
+                   borderpad=0,
+                   )
+fig3.colorbar(im, cax=axins, orientation="horizontal",ticks=[0])
+ax4.annotate(r'$-$',[0.2,-0.32],xycoords='axes fraction',fontsize=12)
+ax4.annotate(r'$+$',[0.8,-0.32],xycoords='axes fraction',fontsize=12)
 
-ax5.plot(map(mean_runlength_normalized,sequence_all[pos]))
-ax5.set_ylabel('Mean run length')
-ax5.set_xlabel('Substitution number')
+plt.tight_layout()
 
-fig1.set_tight_layout()
-
-#ax2.imshow(sequence_all[long_path_pos[0]])
-
-fig1, ax1 = plt.subplots(nrows=1,ncols=1,figsize=[3.5,3.5])
-ax1.imshow(sequence_all[long_path_pos[0]])
-ax1.set_xlabel('Sequence position')
-ax1.set_ylabel('Substitution number')
+#ax4.plot(map(lambda x: dispersion_all_phases(x,3),sequence_all[pos]))
+#ax4.set_ylabel('Clustering')
+#ax4.set_xticklabels([])
+#
+#ax5.plot(map(mean_runlength_normalized,sequence_all[pos]))
+#ax5.set_ylabel('Mean run length')
+#ax5.set_xlabel('Substitution number')
 
 
 
@@ -363,7 +397,7 @@ for _ in not_incomplete_complete_pos:
 for _ in complete_pos:
     plt.plot(np.sum(np.array(structure_all[_])[:path_length(structure_all[_])+1,:2],1),c='C3',linewidth=1.,alpha=0.5)
 
-plt.xlim([0,100])
+plt.xlim([0,200])
 
 
 for _ in not_complete_pos:
